c47465b1a165d0d70f6c7136ff6576fe7ef8a4df
thomas-organ93 <t.organ101@gmail.com>
2026-05-21 14:42:42 +0100

    Footer fix for project pages

--- js/global.js
+++ js/global.js
@@ -4,7 +4,7 @@ if (localStorage.getItem("displayMode") === null) {
 }
 
 // Add footer and auto update copyright year
-fetch('footer.html')
+fetch('/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer-placeholder').innerHTML = data;
